ec825d5adc7b4fc987fb3dc4343a5a140d1cae60
Davi Marques <davimgcb@gmail.com>
2021-12-21 21:47:25 -0300

    a

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ import { resolve } from "path"
 
 const app = express()
 
+
+
 app.use('/', express.static(
-  resolve(
-    __dirname,
+  
     './build'
   )
-))
+)
 
 app.listen(process.env.PORT || 3000, (err)=>{
   if (err){ return console.log(err)}
